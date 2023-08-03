cc786a325bb65ccacf309224c8e1c6b4c3242ed3
Ryan Jeffares <ryandjeffares@gmail.com>
2023-08-03 16:02:31 +0100

    added comments to old version of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "Synth/Oscillator.hpp"
-
 #include <fmt/core.h>
 
 #include <rtaudio/RtAudio.h>
@@ -10,15 +8,17 @@
 #include <random>
 #include <thread>
 
-static Oscillator s_oscillator;
+static std::random_device s_Rd;
+static std::mt19937 s_Gen{s_Rd()};
+static std::uniform_real_distribution<float> s_Dist(-1.0f, 1.0f);
 
-int audioCallback(
-        void* outBuffer,
-        [[maybe_unused]] void* inBuffer,
-        uint32_t bufferFrames,
-        [[maybe_unused]] double streamTime,
-        RtAudioStreamStatus status,
-        [[maybe_unused]] void* userData
+int audio_callback(
+    void* outBuffer,
+    [[maybe_unused]] void* inBuffer,
+    uint32_t bufferFrames,
+    [[maybe_unused]] double streamTime,
+    RtAudioStreamStatus status,
+    [[maybe_unused]] void* userData
 )
 {
     float* buffer = (float*)outBuffer;
@@ -28,10 +28,11 @@ int audioCallback(
         return 0;
     }
 
+    
+    // Samples are interleaved, we can fill both channels with white noise (random numbers)
     for (size_t sample = 0; sample < bufferFrames; sample++) {
-        auto value = s_oscillator.getNextSample();
         for (size_t channel = 0; channel < 2; channel++) {
-            *buffer++ = value * 0.5f;
+            *buffer++ = s_Dist(s_Gen) * 0.5f;
         }
     }
 
@@ -40,36 +41,41 @@ int audioCallback(
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
 {
+    
+    // Object that will handle I/O
     RtAudio dac;
 
+    
+    // Reusable lambda we can use to clean up on error/exit
     auto cleanup = [&] {
         if (dac.isStreamOpen()) {
             dac.closeStream();
         }
     };
 
-    auto deviceIds = dac.getDeviceIds();
-    if (deviceIds.empty()) {
+    auto device_ids = dac.getDeviceIds();
+    if (device_ids.empty()) {
         fmt::print(stderr, "No audio devices found.\n");
         std::exit(1);
     }
 
-    auto defaultDevice = dac.getDefaultOutputDevice();
     
-    RtAudio::StreamParameters streamParameters {
-        .deviceId = defaultDevice,
+    // Set up stream parameters
+    auto default_device = dac.getDefaultOutputDevice();
+
+    RtAudio::StreamParameters stream_parameters {
+        .deviceId = default_device,
         .nChannels = 2,
         .firstChannel = 0
     };
 
     uint32_t sr = 44100;
-    uint32_t bufferSize = 256; 
+    uint32_t buffer_size = 256; 
     
-    s_oscillator.prepare(sr);
-    s_oscillator.setShape(Oscillator::Shape::Saw);
-    s_oscillator.setFrequency(220.0f);
-
-    if (dac.openStream(&streamParameters, nullptr, RTAUDIO_FLOAT32, sr, &bufferSize, &audioCallback)) {
+    // openStream() takes a pointer to the buffer size because it will change it if
+    // that buffer size is not allowed.
+    // Try and open a stream with our desired parameters.
+    if (dac.openStream(&stream_parameters, nullptr, RTAUDIO_FLOAT32, sr, &buffer_size, &audio_callback)) {
         fmt::print(stderr, "{}\n", dac.getErrorText());
         std::exit(1);
     }
@@ -80,6 +86,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
         std::exit(1);
     }
 
+    
+    // For now, just sleep the main thread for 10 seconds so we can hear the audio.
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(10s);
 
@@ -89,4 +97,3 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
 
     cleanup();
 }
-
